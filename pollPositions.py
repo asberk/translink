d1afa31f61cbd5db5b5e53257575c5cfc80beabb
Aaron Berk <aaron.riot1@gmail.com>
2017-06-27 18:41:54 -0700

    [IC] fetching posiiton updates from TransLink API

---
+++ pollPositions.py
@@ -0,0 +1,74 @@
+import pandas as pd
+from google.protobuf import json_format
+import requests
+from google.transit import gtfs_realtime_pb2
+from datetime import datetime
+from matplotlib.pyplot import pause
+
+
+def tryIntCoerce(x):
+    try:
+        x = int(x)
+    except ValueError as ve:
+        pass
+    return x
+
+
+def getLatLon(pu):
+    return tuple(pu['position'][x] for x in ['latitude', 'longitude'])
+
+
+def getTimestamp(pu):
+    return tryIntCoerce(pu['timestamp'])
+
+
+def getTripId(pu):
+    return pu['trip']['tripId']
+
+
+def getVehicleId(pu):
+    return pu['vehicle']['id']
+
+
+def convertColumnToType(df, colTypeDict):
+    for col, newType in colTypeDict.items():
+        df[col] = df[col].astype(newType)
+    return df
+
+
+def pollPositionUpdates(apikey, baseURL):
+    feed = gtfs_realtime_pb2.FeedMessage()
+    response = requests.get(baseURL + apikey)
+    feed.ParseFromString(response.content)
+    position_updates = [json_format.MessageToDict(entity.vehicle)
+                        for entity in feed.entity
+                        if entity.HasField('vehicle')]
+    puCols = ['lat', 'lon', 'ts', 'tid', 'vid']
+    position_updates_df = pd.DataFrame([[*getLatLon(pu), getTimestamp(pu),
+                                         getTripId(pu), getVehicleId(pu)]
+                                        for pu in position_updates],
+                                       columns=puCols)
+    position_updates_df = convertColumnToType(position_updates_df,
+                                              {'tid': 'category',
+                                               'vid': 'category'})
+    return position_updates_df
+
+
+def main(apikey, positionUpdateURL, p=150):
+    while True:
+        print('fetching positions at {}.'.format(datetime.now().isoformat()))
+        pu = pollPositionUpdates(apikey, positionUpdateURL)
+        print(pu.head())
+        print('writing positions to file...')
+        with open('./PositionUpdates.csv', 'a', encoding='utf-8') as fp:
+            pu.to_csv(fp, index=False, header=False)
+        print('waiting for next update...\n')
+        pause(p)
+
+
+if __name__ == '__main__':
+    print('Polling TransLink API for bus positions')
+    with open('./api.key', 'r+', encoding='utf-8') as fp:
+        apikey = fp.read()
+    positionUpdateURL = 'http://gtfs.translink.ca/gtfsposition?apikey='
+    main(apikey, positionUpdateURL)
